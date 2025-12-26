b36dc24dcb6ab8b6fb83bda94c28883fac69a62a
Haroon Saeed <haroon.saeed@outlook.com>
2025-12-26 23:47:00 +0100

    persist posts in local storage

--- src/contexts/PostProvider.jsx
+++ src/contexts/PostProvider.jsx
@@ -1,24 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { PostContext } from "./PostContext";
 function PostProvider(props) {
-  const [posts, setPosts] = useState([
-    {
-      id: 1,
-      title: "Hello World",
-      content: "Welcome to a new blog",
-    },
-    {
-      id: 2,
-      title: "Learn React",
-      content: "React is a JavaScript library for building user interfaces.",
-    },
-    {
-      id: 3,
-      title: "Technologies",
-      content:
-        "React, Node.js, Express.js, MongoDB are good technologies for web development.",
-    },
-  ]);
+  const [posts, setPosts] = useState(() => {
+    const storedPosts = localStorage.getItem("posts");
+    if (storedPosts) {
+      return JSON.parse(storedPosts);
+    } else {
+      return [
+        {
+          id: 1,
+          title: "Learn React",
+          content:
+            "React is a JavaScript library for building user interfaces.",
+        },
+        {
+          id: 2,
+          title: "Technologies",
+          content:
+            "React, Node.js, Express.js, MongoDB are good technologies for web development.",
+        },
+      ];
+    }
+  });
+
+  useEffect(() => {
+    localStorage.setItem("posts", JSON.stringify(posts));
+  }, [posts]);
 
   function addPost(newPost) {
     const lastId = posts[posts.length - 1].id;
